67986c854fc3d5b928c6de781899a8018823dc2c
xlzhen-940218 <xionglongzhen@gmail.com>
2018-12-11 21:14:34 +0800

    remove sql password

--- src/Config.java
+++ src/Config.java
@@ -1,6 +1,6 @@
 
 public class Config {
 	static final String server_sql_path = "jdbc:mysql://localhost:3306/mysql?serverTimezone=GMT%2B8";
-	static final String server_sql_user = "root";
-	static final String server_sql_pwd = "Zxwei123456!@#";
+	static final String server_sql_user = "";
+	static final String server_sql_pwd = "";
 }
